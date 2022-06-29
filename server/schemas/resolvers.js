a740745658d059443126bb5e99b55b4e37d6c03f
Makda Girmay <makdagirmay4@gmail.com>
2022-06-29 13:24:29 -0400

    adding the typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require('../models');
+const resolvers = {
+    Query: {
+      thoughts:async (parent, { username}) => { 
+        const params = username ? { username } : {};
+        return Thought.find().sort({ createAt: -1 });
+      },
+    }
+  };
+  
+  module.exports = resolvers;
